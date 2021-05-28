9e6917e9ea21cba625203543505e8ebf36d4329a
duno <dunonoshiwu@gmail.com>
2021-05-28 21:14:09 +0900

    delete p method for debug, change array name, use sprintf method to adjugest date

--- cal.rb
+++ cal.rb
@@ -5,7 +5,6 @@ require 'optparse'
 
 today = Date.today
 params = ARGV.getopts("", "y:#{today.year}","m:#{today.month}")
-#p params
 
 # 引数なければ今日の日付が入ったオブジェクト使う
 if params["y"] == nil
@@ -15,23 +14,21 @@ else
 end
 end_of_month = Date.new(day.year, day.month, -1)
 first_of_month = Date.new(day.year, day.month, 1)
-array = [*1..end_of_month.day]
-#日付の配列の１桁日付にスペースで幅増やしてる
-array2 = array.map { |x|
-  if x < 10
-    " #{x}"
-  else
-    x
-  end
-}
+date = [*1..end_of_month.day] #今月の日付が入ってる配列
+
+#日付の配列の１桁日付を２桁に調整
+fix_date = date.map do |x|
+  sprintf('%2d', x)
+end
+
 #西暦と月
 print"　　　#{day.month}月 #{day.year}\n"
 #曜日の表示
 print"日 月 火 水 木 金 土\n"
 #日付部分
 day_of_the_week = first_of_month.wday
-print "   " * day_of_the_week#ついたちが何曜日かでスペースの数調整した
-array2.each do |x|
+print "   " * day_of_the_week #ついたちが何曜日かでスペースの数調整した
+fix_date.each do |x|
   new_day = Date.new(day.year, day.month, x.to_i)
   if new_day.saturday? #土曜日だったら改行
     print "#{x}\n"
